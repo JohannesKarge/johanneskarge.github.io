765f50e19966c737a5acdbc9dc208acd519d69b2
JohannesKarge <42804833+JohannesKarge@users.noreply.github.com>
2025-08-03 16:05:53 +0000

    Deploying to gh-pages from @ JohannesKarge/johanneskarge.github.io@e3dd8a88eb524ff23fe73d2cc0b26d3beba70e3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My curriculum vitae highlighting my academic background and professional experience in economics and finance.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
